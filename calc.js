0fa336600bc2d8f7c4fc88edac9dc0dc5ae2c030
Luis <luis.hansen@usp.br>
2017-10-23 10:59:59 -0200

    feat(new): added ops
    Added "Change operations" and "Store".

--- calc.js
+++ calc.js
@@ -1,5 +1,4 @@
 "use strict";
-let count = 0;
 
 String.prototype.replaceAll = function(search, replacement) {
     var target = this;
@@ -24,105 +23,282 @@ Array.prototype.rotate = (function() {
     };
 })();
 
-let getNS = (op, num) => {
-	count ++;
-	var buf;
-	var operand;
-	var regex;
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+}
 
-	// Multiplication
-	regex = /x(-*\d+)/ig;
-	if (buf = regex.exec(op)) {
-		operand = buf[1];
-		return (Number(num)*Number(operand)).toString();
-	}
+let updateOps = (ops, operation, num) => {
+	ops.forEach((op) => {
+		switch (operation) {
+			case "x":
+			case "X":
+				op.string = op.string.replaceAll(op.num, (Number(op.num) * Number(num)).toString());
+				op.num = op.num ? (Number(op.num) * Number(num)).toString() : undefined;
+				op.num1 = op.num1 ? (Number(op.num1) * Number(num)).toString() : undefined;
+				op.num2 = op.num2 ? (Number(op.num2) * Number(num)).toString() : undefined;
+			break;
 
-	// Division
-	regex = /\/(-*\d+)/ig;
-	if (buf = regex.exec(op)) {
-		operand = buf[1];
-		return (Number(num)/Number(operand)).toString();
-	}
+			case "+":
+				op.string = op.string.replaceAll(op.num, (Number(op.num) + Number(num)).toString());
+				op.num = op.num ? (Number(op.num) + Number(num)).toString() : undefined;
+				op.num1 = op.num1 ? (Number(op.num1) + Number(num)).toString() : undefined;
+				op.num2 = op.num2 ? (Number(op.num2) + Number(num)).toString() : undefined;
+			break;
 
-	// Sum
-	regex = /\+(\d+)/ig;
-	if (buf = regex.exec(op)) {
-		operand = buf[1];
-		return (Number(num)+Number(operand)).toString();
-	}
+			case "-":
+				op.string = op.string.replaceAll(op.num, (Number(op.num) - Number(num)).toString());
+				op.num = op.num ? (Number(op.num) - Number(num)).toString() : undefined;
+				op.num1 = op.num1 ? (Number(op.num1) - Number(num)).toString() : undefined;
+				op.num2 = op.num2 ? (Number(op.num2) - Number(num)).toString() : undefined;
+			break;
 
-	// Subtraction
-	regex = /^-(\d+)/ig;
-	if (buf = regex.exec(op)) {
-		operand = buf[1];
-		return (Number(num)-Number(operand)).toString();
-	}
-
-	// PUT num
-	regex = /put\s*(\d+)/ig;
-	if (buf = regex.exec(op)) {
-		operand = buf[1];
-		return num + "" + operand;
-	}
-
-	// => fn
-	regex = /(-*\d*)=>(-*\d*)/ig;
-	if (buf = regex.exec(op)) {
-		return num.toString().replaceAll(buf[1], buf[2]);
-	}
-
-	// <<
-	if (op === "<<") {
-		return (Math.floor(num/10)).toString();;
-	}
-
-	// +-
-	if (op === "+-" || op === "-+") {
-		return (-1*num).toString();;
-	}
-
-	// "SUM"
-	if (op === "SUM" || op === "sum") {
-		return eval(num.toString().replace(/(\d)(?=\d)/g, '$1+')).toString();
-	}
-
-	// REVERSE
-	if (op === "REVERSE" || op === "reverse") {
-		if ( num >= 0 ) {
-			return num.toString().split('').reverse().join('').toString();
-		} else {
-			return (-1*(-1*num).toString().split('').reverse().join('')).toString();
+			case "/":
+				op.string = op.string.replaceAll(op.num, (Number(op.num) / Number(num)).toString());
+				op.num = op.num ? (Number(op.num) / Number(num)).toString() : undefined;
+				op.num1 = op.num1 ? (Number(op.num1) / Number(num)).toString() : undefined;
+				op.num2 = op.num2 ? (Number(op.num2) / Number(num)).toString() : undefined;
+			break;
 		}
-	}
+	});
+}
 
-	// SHIFT RIGHT
-	if (op === "shift>" || op === "SHIFT>") {
-		if ( num >= 0 ) {
-			return num.toString().split('').rotate(-1).join('').toString();
-		} else {
-			return (-1*(-1*num).toString().split('').rotate(-1).join('')).toString();
-		}
-	}
+let evalOps = (ops) => {
+	let newOps = ops.map((op) => {
+		let buf;
+		let opObj = { string: op };
+		let regex;
 
-	// SHIFT LEFT
-	if (op === "<shift" || op === "<SHIFT") {
-		if ( num >= 0 ) {
-			return num.toString().split('').rotate(1).join('').toString();
-		} else {
-			return (-1*(-1*num).toString().split('').rotate(1).join('')).toString();
+		// Multiplication
+		regex = /x(-*\d+)/ig;
+		if (buf = regex.exec(op)) {
+			opObj.operation = "mul";
+			opObj.num = buf[1];
+			return opObj;
 		}
-	}
 
-	// MIRROR
-	if (op === "mirror" || op === "MIRROR") {
-		if ( num >= 0 ) {
-			return num.toString() + num.toString().split('').reverse().join('').toString();
-		} else {
-			return num.toString() + (-1*(-1*num).toString().split('').reverse().join('')).toString();
+		// Division
+		regex = /\/(-*\d+)/ig;
+		if (buf = regex.exec(op)) {
+			opObj.operation = "div";
+			opObj.num = buf[1];
+			return opObj;
 		}
+
+		// Sum
+		regex = /\+(\d+)/ig;
+		if (buf = regex.exec(op)) {
+			opObj.operation = "sum";
+			opObj.num = buf[1];
+			return opObj;
+		}
+
+		// Subtraction
+		regex = /^-(\d+)/ig;
+		if (buf = regex.exec(op)) {
+			opObj.operation = "sub";
+			opObj.num = buf[1];
+			return opObj;
+		}
+
+		// PUT num
+		regex = /put\s*(\d+)/ig;
+		if (buf = regex.exec(op)) {
+			opObj.operation = "put";
+			opObj.num = buf[1];
+			return opObj;
+		}
+
+		// => fn
+		regex = /(-*\d*)=>(-*\d*)/ig;
+		if (buf = regex.exec(op)) {
+			opObj.operation = "fn";
+			opObj.num1 = buf[1];
+			opObj.num2 = buf[2];
+			return opObj;
+		}
+
+		// <<
+		if (op === "<<") {
+			opObj.operation = "<<";
+			return opObj;
+		}
+
+		// +-
+		if (op === "+-" || op === "-+") {
+			opObj.operation = "opo";
+			return opObj;
+		}
+
+		// "SUM"
+		if (op === "SUM" || op === "sum") {
+			opObj.operation = "sal";
+			return opObj;
+		}
+
+		// REVERSE
+		if (op === "REVERSE" || op === "reverse") {
+			opObj.operation = "rev";
+			return opObj;
+		}
+
+		// SHIFT RIGHT
+		if (op === "shift>" || op === "SHIFT>") {
+			opObj.operation = "sh>";
+			return opObj;
+		}
+
+		// SHIFT LEFT
+		if (op === "<shift" || op === "<SHIFT" || op === "shift<" || op === "SHIFT<") {
+			opObj.operation = "sh<";
+			return opObj;
+		}
+
+		// MIRROR
+		if (op === "MIRROR" || op === "mirror") {
+			opObj.operation = "mir";
+			return opObj;
+		}
+
+		// CHANGE OPS
+		regex = /\[([x\+-\/])\](\d+)/ig;
+		if (buf = regex.exec(op)) {
+			opObj.operation = "cop";
+			opObj.copOp = buf[1];
+			opObj.copNum = buf[2];
+			return opObj;
+		}
+
+		// STORE
+		if (op === "STORE" || op === "store") {
+			opObj.operation = "strl";
+			return opObj;
+		}
+	});
+
+	newOps.forEach((op) => {
+		if (op.operation === "strl") {
+			op.string = "Store (long hold)";
+			newOps.push({
+				string: "Store (single click)",
+				operation: "str",
+				num: null
+			});
+		}
+	});
+
+	return newOps;
+}
+
+let store = (num, ops) => {
+	ops.forEach((op) => {
+		if (op.operation === "str") {
+			if (num >= 0) {
+				op.num = num.toString();
+			} else {
+				op.num = (-1*Number(op.num)).toString();
+			}
+		}
+	});
+}
+
+let getNS = (op, num, ops) => {
+
+	let operand = op.num ? op.num : undefined;
+
+	switch (op.operation) {
+
+		case "mul":
+			return (Number(num)*Number(operand)).toString();
+		break;
+
+		case "div":
+			return (Number(num)/Number(operand)).toString();
+		break;
+
+		case "sum":
+			return (Number(num)+Number(operand)).toString();
+		break;
+
+		case "sub":
+			return (Number(num)-Number(operand)).toString();
+		break;
+
+		case "put":
+			return num + "" + operand;
+		break;
+
+		case "fn":
+			return num.toString().replaceAll(op.num1, op.num2);
+		break;
+
+		case "<<":
+			return (Math.floor(num/10)).toString();
+		break;
+
+		case "opo":
+			return (-1*num).toString();
+		break;
+
+		case "sal":
+			return eval(num.toString().replace(/(\d)(?=\d)/g, '$1+')).toString();
+		break;
+
+		case "rev":
+			if ( num >= 0 ) {
+				return num.toString().split('').reverse().join('').toString();
+			} else {
+				return (-1*(-1*num).toString().split('').reverse().join('')).toString();
+			}
+		break;
+
+		case "sh>":
+			if ( num >= 0 ) {
+				return num.toString().split('').rotate(-1).join('').toString();
+			} else {
+				return (-1*(-1*num).toString().split('').rotate(-1).join('')).toString();
+			}
+		break;
+
+		case "sh<":
+			if ( num >= 0 ) {
+				return num.toString().split('').rotate(1).join('').toString();
+			} else {
+				return (-1*(-1*num).toString().split('').rotate(1).join('')).toString();
+			}
+		break;
+
+		case "mir":
+			if ( num >= 0 ) {
+				return num.toString() + num.toString().split('').reverse().join('').toString();
+			} else {
+				return num.toString() + (-1*(-1*num).toString().split('').reverse().join('')).toString();
+			}
+		break;
+
+		case "cop":
+			updateOps(ops, op.copOp, op.copNum);
+			return num;
+		break;
+
+		case "strl":
+			store(num, ops);
+			return num;
+		break;
+
+		case "str":
+			return num + (operand ? operand : "");
+		break;
+
 	}
 
 }
+
+let his = [];
 
 function solve(ops, movs, init, final, history = [init]) {
 	let my_ops = arguments[0];
@@ -130,18 +306,31 @@ function solve(ops, movs, init, final, history = [init]) {
 	let my_init = arguments[2];
 	let my_final = arguments[3];
 
+	if (my_ops.len === 0) {
+		console.log("No operations!");
+		return;
+	}
+
+	if (typeof my_ops[0] != "object") {
+		my_ops = evalOps(my_ops);
+	}
+
 	if (my_movs === 0) {
 		history.push(my_init);
+		his.push(history.join(' -> '));
 		if (my_init == my_final) {
 			console.log(history.join(' -> '));
 		}
 		return my_init;
 	} else {
 		for (let i=0;i<my_ops.length;i++) {
-			let newHistory = history.slice();
-			newHistory.push(ops[i]);
-			let a = getNS(ops[i], my_init);
-			solve(ops, my_movs-1, a, my_final, newHistory);
+			if (!(my_ops[i].operation === "strl" && history[history.length-1] === "Store (long hold)")) {
+				let newHistory = history.slice();
+				newHistory.push(my_ops[i].string);
+				let newOps = my_ops.map((val)=>clone(val));
+				let nextInit = getNS(my_ops[i], my_init, newOps);
+				solve(newOps, ((my_ops[i].operation === "strl") ? my_movs : my_movs-1), nextInit, my_final, newHistory);
+			}
 		}
 	}
 }
