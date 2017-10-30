3fe5d2c8e8e88a11dbfd0d688411cd1ace7ae2e8
LuisHansen <reescolando@gmail.com>
2017-10-30 10:54:06 -0200

    Portals interface added. Everything wokrs fine now. Every level is solvable.

--- form.js
+++ form.js
@@ -5,13 +5,30 @@ formSolve = () => {
 	mov = Number(document.querySelector("form").mov.value);
 	init = Number(document.querySelector("form").init.value);
 	final = Number(document.querySelector("form").final.value);
+	portalin = Number(document.querySelector("form").in.value);
+	portalout = Number(document.querySelector("form").out.value);
 
 	var linebreak = document.createElement("br");
 	document.querySelector("#solutionBox").append("--------------------");
 	document.querySelector("#solutionBox").appendChild(linebreak);
 
-	solve(ops, mov, init, final);
+	solve(ops, mov, init, final, ((portalin != null) && (portalout != null)) ? {in: portalin, out: portalout} : undefined);
 
+	return false;
+}
+
+clearPortals =() => {
+	form = document.querySelector("form");
+	form.in.forEach( (input) => {
+	    if (input.checked == true) {
+	        input.checked = false;
+	    }
+	});
+	form.out.forEach( (input) => {
+	    if (input.checked == true) {
+	        input.checked = false;
+	    }
+	});
 	return false;
 }
 
